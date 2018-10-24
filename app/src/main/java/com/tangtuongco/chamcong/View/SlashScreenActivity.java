e0933fc09d56739a31ff027d0844466e9f61107d
Co Tsang <covippro1@gmail.com>
2018-10-24 18:56:38 +0700

    24/10/2018

---
+++ app/src/main/java/com/tangtuongco/chamcong/View/SlashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.tangtuongco.chamcong.View;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tangtuongco.chamcong.R;
+
+public class SlashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_slashscreen);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iDangNhap = new Intent(SlashScreenActivity.this,DangNhap.class);
+                startActivity(iDangNhap);
+                finish();
+
+
+            }
+        },2000);
+    }
+}
